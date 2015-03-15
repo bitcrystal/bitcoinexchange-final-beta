b6fe1cddffcb771c2ee5a376dbeb2af2cfe85743
root <root@debian>
2015-03-15 16:16:45 +0100

    update100

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 $dbdb_host = 'localhost';         // the server the database is located
-$dbdb_user = 'bitcoinexchange2';      // the username to access the database
+$dbdb_user = 'bitcoinexchange3';      // the username to access the database
 $dbdb_pass = 'fickdiehennecool';      // the password to access the database
-$dbdb_database = 'bitcoinexchange2';  // the name of the database being used
+$dbdb_database = 'bitcoinexchange3';  // the name of the database being used
 ?>
